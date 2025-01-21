225e3f7d8bb8c95cb264a2766006226183469500
odv144 <omar.virili@gmail.com>
2025-01-20 21:51:40 -0300

    Setup React boilerplate

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Button, Center } from "@chakra-ui/react"
+
+
+function App() {
+ 
+
+  return (
+    <Center bg='red'>
+    <p>Hola Mundo</p>
+    <Button >Click me</Button>
+    </Center>
+  )
+}
+
+export default App
