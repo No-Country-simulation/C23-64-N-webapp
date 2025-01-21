088b47e96ce87bc1d1649160223d19005969787d
odv144 <omar.virili@gmail.com>
2025-01-21 11:32:44 -0300

    feat: implement react router dom and structure home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
-import { Button, Center } from "@chakra-ui/react"
-
+import { Button, Center } from "@chakra-ui/react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home";
+import { Footer } from "./components/Footer/Footer";
+import Contacto from "./pages/Contacto";
 
 function App() {
- 
-
   return (
-    <Center bg='red'>
-    <p>Hola Mundo</p>
-    <Button >Click me</Button>
-    </Center>
-  )
+    <BrowserRouter>
+     <Header/>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/contacto" element={<Contacto />}></Route>
+        {/* <Route path="/player/:id" element={<Player />}></Route> */}
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
