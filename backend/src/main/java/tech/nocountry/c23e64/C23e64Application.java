c535db8d0b2812ba57ae8ef94042670cf80ed1a4
Whiskydog <alan.varde@gmail.com>
2025-01-21 09:37:53 -0300

    Add Spring Boot boilerplate

---
+++ backend/src/main/java/tech/nocountry/c23e64/C23e64Application.java
@@ -0,0 +1,13 @@
+package tech.nocountry.c23e64;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C23e64Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C23e64Application.class, args);
+	}
+
+}
