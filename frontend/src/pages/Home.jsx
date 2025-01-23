3dff7ea50cd2d3cdb2d9abbb80e63a9e9aaa7c2d
odv144 <omar.virili@gmail.com>
2025-01-23 19:42:37 -0300

    fix home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,53 +1,43 @@
-import { Button, Center, Container, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { Button, Center, Container, Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
 import React, { useContext } from "react";
-import Cards from "../components/Card/Cards";
+
 import Carousel from "../components/Carrusel/Carousel";
 import { MuebleContext } from "../Context/MuebleContext";
+import CategoryCard from "../components/Card/CategoryCard";
 
 
 const Home = () => {
-  const data=[
-    {
-      id:1,
-    title:'Sofa para Sala de estar',
-    precio:'14.500',
-    img:'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
-    descripcion:'Este sofa es perfecto para espacios modernos tropicales, espacios inspirados en el barroco, espacios de tonos terrosos y para personas que aman un diseño chic con un toque de diseño vintage.',
-  },
-  {
-    id:2,
-    title:'Otro Sofa',
-    precio:'12.500',
-    img:'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
-    descripcion:'Este sofa  espacios de tonos terrosos y para personas que aman un diseño chic con un toque de diseño vintage.',
-  },
-]
+  const { furniture} = useContext(MuebleContext);
 
-const img=[
-  
-    'https://lh4.googleusercontent.com/proxy/6sovWP55KGpgDxoLU2M_Y5JMQnNuSkJ9sr9pGgeLW86GZIXymSmb92_AixZhg1P4iXBGLZFiEZeglnrDJ4YrMqVR7yLeEolg55DUz_d5t7j4rM7sIoZwyyTgpC_aNjeUXRh0TdzlC4E8clY_-qgBZ7DmvNmOBvf9VgIsPDanlw',
-    'https://img.freepik.com/fotos-premium/muebles-mimbre-marron-encuentran-cesped-exterior-dia-soleado-verano_615536-394.jpg',
-    'https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
-   
-]
-const {muebles,setMuebles}=useContext(MuebleContext)
 
+  const img = [
+    "https://lh4.googleusercontent.com/proxy/6sovWP55KGpgDxoLU2M_Y5JMQnNuSkJ9sr9pGgeLW86GZIXymSmb92_AixZhg1P4iXBGLZFiEZeglnrDJ4YrMqVR7yLeEolg55DUz_d5t7j4rM7sIoZwyyTgpC_aNjeUXRh0TdzlC4E8clY_-qgBZ7DmvNmOBvf9VgIsPDanlw",
+    "https://img.freepik.com/fotos-premium/muebles-mimbre-marron-encuentran-cesped-exterior-dia-soleado-verano_615536-394.jpg",
+    "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+  ];
+  // const {muebles,setMuebles}=useContext(MuebleContext)
+  const typeProducts = furniture.filter((item, index, self) => 
+    index === self.findIndex(t => t.type === item.type)
+);
+// console.log("productos",typeProducts)
   return (
-    <VStack h={'100%'} justifyContent={'center'}>
+    <VStack h={"100%"} justifyContent={"center"}>
       <Carousel images={img} />
-      
-      <HStack>
-        {data.map((item)=>
-        <Cards key={item.id} 
-          title={item.title}
-          precio={item.precio}
-          img={item.img}
-          descripcion={item.descripcion}
+      <Heading as="h1" size='2xl'>CATEGORIAS</Heading>
+      <HStack wrap={'wrap'} justifyContent={"center"} spacing={4}>
+        {furniture.map((item) => (
+          <CategoryCard
+            key={item.id}
+            id={item.id}
+            title={item.name}
+            img={item.imageUri}
+           
           />
-          )}
+        ))}
       </HStack>
     </VStack>
   );
 };
+
 
 export default Home;
