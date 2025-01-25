afb679a60e8c8bbc563d1e70e7d4ace7b1b59ee4
Whiskydog <alan.varde@gmail.com>
2025-01-25 18:28:59 -0300

    refactor: decouple FurnitureMapper from category repository

--- backend/src/main/java/tech/nocountry/c23e64/mapper/CategoryMapper.java
+++ backend/src/main/java/tech/nocountry/c23e64/mapper/CategoryMapper.java
@@ -1,16 +1,16 @@
 package tech.nocountry.c23e64.mapper;
 
 import org.mapstruct.Mapper;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapping;
 import tech.nocountry.c23e64.dto.CategoryCreateDto;
 import tech.nocountry.c23e64.dto.CategoryDto;
 import tech.nocountry.c23e64.model.CategoryEntity;
-import tech.nocountry.c23e64.repository.CategoryRepository;
 
 @Mapper(componentModel = "spring")
 public interface CategoryMapper {
 
     CategoryDto toDto(CategoryEntity categoryEntity);
 
+    @Mapping(target = "id", ignore = true)
     CategoryEntity toEntity(CategoryCreateDto createDto);
 }
